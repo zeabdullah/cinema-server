b8b7aaf0f66a7881895d17de5b8f36e1ce806e3a
Abdullah Z <dev@zabd.dev>
2025-06-28 18:38:21 +0300

    feat: create and test migration 001

---
+++ migrations/001_init_tables.php
@@ -0,0 +1,33 @@
+<?php
+require('../connection/connection.php');
+
+$sql =
+    'CREATE TABLE IF NOT EXISTS films(
+        id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        genre VARCHAR(255) NOT NULL,
+        trailer_url VARCHAR(255),
+        duration INT(3) NOT NULL,
+        description TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+
+    CREATE TABLE IF NOT EXISTS users(
+        id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+';
+
+$firstSuccess = $mysqli->multi_query($sql);
+if ($firstSuccess) {
+    echo 'first ok' . '<br>';
+}
+while ($mysqli->next_result()) {
+    echo 'ok' . '<br>';
+}
+
