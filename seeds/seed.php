63da2f0194b5fce668840574d212b22cc995cc13
Abdullah Z <dev@zabd.dev>
2025-06-29 01:00:40 +0300

    feat: implement login controller

--- seeds/seed.php
+++ seeds/seed.php
@@ -1,21 +1,24 @@
 <?php
-require('../models/User.php');
+require '../models/User.php';
 
 function seed_users()
 {
     $usersToCreate = [
         [
             'email' => 'admin@example.com',
+            'password' => 'admin@example.com',
             'first_name' => 'Mark',
             'last_name' => 'Admin',
         ],
         [
             'email' => 'admin2@example.com',
+            'password' => 'admin2@example.com',
             'first_name' => 'Mike',
             'last_name' => 'Admin',
         ],
         [
             'email' => 'user@example.com',
+            'password' => 'user@example.com',
             'first_name' => 'Owen',
             'last_name' => 'User',
         ]
@@ -24,13 +27,14 @@ function seed_users()
     foreach ($usersToCreate as $u) {
         User::create($u);
     }
-    echo 'Users seeding run successfully!' . '<br>';
 }
 
 function seed_db()
 {
     seed_users();
-    echo 'Successfully seeded the database!' . '<br>';
+    echo json_encode([
+        'message' => 'Successfully seeded the database!'
+    ]);
 }
 
 seed_db();
