1037433c1d7e7ca189f7eb12f69fabd8620d4aac
Abdullah Z <dev@zabd.dev>
2025-06-28 18:45:23 +0300

    feat: create base Model class

---
+++ models/Model.php
@@ -0,0 +1,76 @@
+<?php
+abstract class Model
+{
+    protected int $id;
+
+    protected static string $table_name;
+    protected static string $primary_key = "id";
+
+    public function __construct(array $data)
+    {
+        $this->id = $data['id'];
+    }
+
+    public static function findById(int $id)
+    {
+        global $mysqli;
+
+        $sql = sprintf(
+            "Select * from %s WHERE %s = ?",
+            static::$table_name,
+            static::$primary_key
+        );
+
+        $query = $mysqli->prepare($sql);
+        $query->bind_param("i", $id);
+        $query->execute();
+
+        $data = $query->get_result()->fetch_assoc();
+
+        return $data ? new static($data) : null;
+    }
+
+    public static function getAll()
+    {
+        global $mysqli;
+
+        $sql = sprintf("Select * from %s", static::$table_name);
+
+        $query = $mysqli->prepare($sql);
+        $query->execute();
+
+        $data = $query->get_result();
+
+        $objects = [];
+        while ($row = $data->fetch_assoc()) {
+            $objects[] = new static($row);
+        }
+
+        return $objects;
+    }
+
+    public static function create()
+    {
+
+    }
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+        global $mysqli;
+        try {
+            $sql = sprintf("DELETE FROM %s WHERE %s = ?", static::$table_name, static::$primary_key);
+
+            $query = $mysqli->prepare($sql);
+            $query->bind_param("i", $this->id);
+            $query->execute();
+
+            return 'delete success';
+        } catch (\Throwable $th) {
+            return $th->__tostring();
+        }
+    }
+}
